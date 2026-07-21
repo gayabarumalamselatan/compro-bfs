5491a231c0d7bea45bb74797645321af92a5c783
febri <soerjahartono22@gmail.com>
2026-07-21 14:27:53 +0700

    fix: sitemap url

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,27 +1,29 @@
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from "next";
+
+export const dynamic = "force-dynamic";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   // Ganti URL ini dengan domain asli Anda saat naik ke production (contoh: https://bfs.com)
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://192.168.19.188:3000'
-
+  const baseUrl =
+    process.env.NEXT_PUBLIC_BASE_URL || "http://192.168.19.188:3000";
   return [
     {
       url: `${baseUrl}`,
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly",
       priority: 1,
     },
     {
       url: `${baseUrl}/products`,
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly",
       priority: 0.8,
     },
     {
       url: `${baseUrl}/iot-solutions`,
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly",
       priority: 0.8,
     },
-  ]
+  ];
 }
