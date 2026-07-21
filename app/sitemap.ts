880901d7558c68088ddd2b81969d51fc000c496e
febri <soerjahartono22@gmail.com>
2026-07-21 13:29:21 +0700

    chore: adding sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,27 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  // Ganti URL ini dengan domain asli Anda saat naik ke production (contoh: https://bfs.com)
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://192.168.19.188:3000'
+
+  return [
+    {
+      url: `${baseUrl}`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/products`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/iot-solutions`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+  ]
+}
